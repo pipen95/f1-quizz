b1b5d0834262a6958b63b0e5e7637d57e0c663e9
MacBookPro3 <macbookpro3@Little-MacBook-Pro.local>
2020-11-15 21:38:11 -0500

    Gitignore: add build

---
+++ build/precache-manifest.ce20b47e5da7a508cdc5441445f1d45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1d0a5c640d273622b739e001705063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a1048f0cd9dbbd0eca5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0c75b1d8ab305ec54fef",
+    "url": "/static/css/main.8c044b93.chunk.css"
+  },
+  {
+    "revision": "9a1048f0cd9dbbd0eca5",
+    "url": "/static/js/2.004aa9a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.004aa9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c75b1d8ab305ec54fef",
+    "url": "/static/js/main.7107be23.chunk.js"
+  },
+  {
+    "revision": "9a49241bdd2763732be1",
+    "url": "/static/js/runtime-main.db26cafa.js"
+  }
+]);
